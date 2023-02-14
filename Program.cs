6a3466d004007e6b2567e9e1f51ba48d6819f740
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-14 10:15:42 +0530

    Added Fibonacci program

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace FunctionalProgrammingProblems
+﻿using FunctionalProgramming;
+
+namespace FunctionalProgrammingProblems
 {
     public class FunctionalProblems
     {
         public static void Main(string[] args)
         {
+            Fibonacci.printFibonacci();
             
+            //PerfectNumber.findPerfectNumber();
+            //ReverseNum.printReverse();
+            //PrimeNumber.checkPrime();
         }
     }
 }
