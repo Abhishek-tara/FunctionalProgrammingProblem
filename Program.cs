5495fd08afdf0c473501eb3aa2711b48462b2f77
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-15 14:22:35 +0530

    Added Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace FunctionalProgrammingProblems
+﻿using System.Diagnostics;
+
+namespace FunctionalProgrammingProblems
 {
     public class FunctionalProblems
     {
         public static void Main(string[] args)
         {
-            
+            //Fibonacci.printFibonacci();
+            //PerfectNumber.findPerfectNumber();
+            //PrimeNumber.checkPrime();
+            //ReverseNumber.printReverse();
+            //CouponNumbers.distinctCouponNumber();
+            //StopWatch.startStopWatch();
         }
     }
 }
